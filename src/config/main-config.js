2b352f72dfac53d9361c4ee251a4352b444ad3fb
ngalang92 <nickgalangsf@gmail.com>
2018-10-09 18:57:11 -0700

    Working on deployment

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,37 +1,13 @@
 require("dotenv").config();
+const logger = require('morgan');
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
-const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
-const session = require("express-session");
-const flash = require("express-flash");
-const passportConfig = require("./passport-config");
-const logger = require('morgan');
-
 
 module.exports = {
   init(app, express){
-    app.set("views", viewsFolder);
-    app.set("view engine", "ejs");
-    app.use(express.static(path.join(__dirname, "..", "assets")));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(expressValidator());
-    app.use(session({
-      secret: process.env.cookieSecret,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 1.21e+9 } //set cookie to expire in 14 days
-    }));
-    app.use(flash());
-    passportConfig.init(app);
-
-    app.use((req,res,next) => {
-      res.locals.currentUser = req.user;
-      next();
-    });
-
-    app.use(logger('dev'));
-
-    app.use(express.static(path.join(__dirname, "..", "assets")));
+   app.use(logger('dev'));
+   app.set("views", viewsFolder);
+   app.set("view engine", "ejs");
+   app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
--- src/server.js
+++ src/server.js
@@ -20,5 +20,5 @@ function normalizePort(val) {
 }
 
 server.on("listening", () => {
-  console.log("server is listening for requests on port ${server.address().port}");
+  console.log(`server is listening for requests on port ${server.address().port}`);
 });
