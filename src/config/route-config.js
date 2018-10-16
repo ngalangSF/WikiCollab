ff745ff6730fce8c0d21244788ac1f8fed22ed9b
ngalang92 <nickgalangsf@gmail.com>
2018-10-16 16:36:06 -0700

    Wiki_CRUD

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const wikiRoutes = require("../routes/wikis");
     const userRoutes = require("../routes/users");
     app.use(staticRoutes);
-    app.use(userRoutes);    
+    app.use(wikiRoutes);
+    app.use(userRoutes);
   }
 }
---
+++ src/db/migrations/20181016200037-create-wiki.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Wikis', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      private: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Wikis');
+  }
+};
